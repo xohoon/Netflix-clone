cd8b6c1ee1cfb26df41aa15ab1894d59f46b23a4
xohoon <xohoonx2@gmail.com>
2022-06-02 23:36:33 +0900

    home_topbar update

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -12,10 +12,39 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('Home'),
-      ),
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset('assets/img/logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 1),
+            child: const Text('TV 프로그램',
+              style: TextStyle(fontSize: 14)),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 1),
+            child: const Text('영화',
+              style: TextStyle(fontSize: 14)),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 1),
+            child: const Text('내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14)),
+          ),
+        ],
+      )
     );
   }
 }
