461f216553a592110eb1c53982ea0a1210507b61
xohoon <xohoonx2@gmail.com>
2022-06-02 23:08:52 +0900

    Netflix clone coding

---
+++ android/app/src/main/java/com/xohoon/netflix/MainActivity.java
@@ -0,0 +1,6 @@
+package com.xohoon.netflix;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/screen/home.dart';
+import 'package:netflix/widget/bottom_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Home(),
+              Container(child: Text("SEARCH")),
+              Container(child: Text("LIST")),
+              Container(child: Text("MORE")),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+
+}
